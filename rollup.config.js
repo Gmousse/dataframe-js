70bf720a00fd93f26a2449d6d39951340d061e8b
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2019-01-10 19:30:14 +0100

    feat: modify the build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
     input: "./src/index.js",
     output: {
-        file: "./build/dataframe.js",
+        file: "./dist/dataframe.min.js",
         name: "dfjs",
-        format: "umd"
+        format: "iife"
     },
     plugins: [
         resolve(),
@@ -16,6 +17,7 @@ export default {
         }),
         babel({
             exclude: "node_modules/**"
-        })
+        }),
+        uglify()
     ]
 };
